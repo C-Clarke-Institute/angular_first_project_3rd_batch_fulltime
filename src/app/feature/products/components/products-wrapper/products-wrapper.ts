706f10c00c6d9ee07d50b0d25c52ce69cf7b4dde
academic.cci@gmail.com <c-clarke@c-clarkes-Mac-mini.local>
2026-09-10 10:12:44 +0530

    Signal

--- src/app/feature/products/components/products-wrapper/products-wrapper.ts
+++ src/app/feature/products/components/products-wrapper/products-wrapper.ts
@@ -2,21 +2,29 @@ import { Component, signal } from '@angular/core';
 import { ProductDto } from '../../models/product.model';
 import { Product } from '../product/product';
 import { productsList } from '../../utils/products-list';
+import { single } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products-wrapper',
-  imports: [Product],
+  imports: [Product, FormsModule],
   templateUrl: './products-wrapper.html',
   styleUrl: './products-wrapper.scss',
 })
 export class ProductsWrapper {
   products: ProductDto[] = productsList;
-
+  discountPercentage: number = 0;
   cartProducts: ProductDto[] = [];
 
   counter = 0;
   count = signal(0);
+  totalPrice = signal(0);
 
+  showProduct = false;
+
+  public toggleProducts() {
+    this.showProduct = !this.showProduct;
+  }
 
   public onAddToCartProduct(product: ProductDto) {
     this.cartProducts.push(product);
@@ -27,6 +35,7 @@ export class ProductsWrapper {
 
     // update value
     this.counter += 1;
-    this.count.update( value => value + 1);
+    this.count.update((value) => value + 1);
+    this.totalPrice.update((value) => value + product.price);
   }
 }
