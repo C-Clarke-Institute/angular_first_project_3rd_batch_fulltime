d851b8b9bb36ce4d00a980ac81e2f0b730a9ee4b
academic.cci@gmail.com <c-clarke@c-clarkes-Mac-mini.local>
2026-09-24 11:31:35 +0530

    Component LifeCycle

--- src/app/feature/products/components/product/product.ts
+++ src/app/feature/products/components/product/product.ts
@@ -1,4 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { ProductDto } from '../../models/product.model';
 import { Highlight } from '../../../../directives/highlight';
 import { CurrencyPipe, DatePipe, NgClass, NgStyle } from '@angular/common';
@@ -10,17 +20,38 @@ import { TruncatePipe } from '../../../../pipes/truncate-pipe';
   templateUrl: './product.html',
   styleUrl: './product.scss',
 })
-export class Product {
-
+export class Product implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   @Input() product!: ProductDto;
+  @Input() productCount!: number;
 
   @Output() onAddToCart = new EventEmitter();
 
   constructor() {
+    console.log("On Constructor Product");
   }
+
+  ngAfterViewInit(): void {
+    console.log("On After View Product");
+  }
+
+  ngOnDestroy(): void {
+    console.log("On Destroy Product");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("On Changes Product");
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+   console.log("On Initial Product");
+  }
+
+
+
+
 
   public onAddToCartProduct() {
-    this.onAddToCart.emit( this.product );
+    this.onAddToCart.emit(this.product);
   }
-
 }
--- src/app/feature/products/utils/products-list.ts
+++ src/app/feature/products/utils/products-list.ts
@@ -8,21 +8,5 @@ export const productsList: ProductDto[] = [
     price: 120.00,
     stock : 5,
     createdOn: new Date('2026-06-10T10:00:00.000Z'),
-  },
-  {
-    id: 2,
-    name: "Mobile Phone",
-    description: "Mobile Phone Description",
-    price: 200.00,
-    stock : 3,
-    createdOn: new Date()
-  },
-  {
-    id: 3,
-    name: "Laptop",
-    description: "Laptop Description",
-    price: 450.00,
-    stock : 0,
-    createdOn: new Date()
   }
 ]
