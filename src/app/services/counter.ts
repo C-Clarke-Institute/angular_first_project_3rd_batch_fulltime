d401d843a5a61039ceae6b8d284ffe633c99972f
academic.cci@gmail.com <c-clarke@c-clarkes-Mac-mini.local>
2026-09-24 10:53:39 +0530

    Observable and Subscribe

--- src/app/services/counter.ts
+++ src/app/services/counter.ts
@@ -1,4 +1,5 @@
 import { Injectable, signal } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -18,4 +19,25 @@ export class Counter {
   getCount() {
     return this.count();
   }
+
+  numbers$ = new Observable( observable => {
+
+    console.log("counter observable created");
+    observable.next( 1);
+    observable.next( 2);
+    observable.next( 3);
+    observable.complete();
+
+  });
+
+  getCounter(): Observable<number> {
+    return new Observable( observable => {
+       let counter = 0;
+       setInterval( () => {
+         counter++;
+         observable.next(counter);
+       }, 2000 );
+    })
+  }
+
 }
--- src/app/side-nav-bar/side-nav-bar.ts
+++ src/app/side-nav-bar/side-nav-bar.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SideNavAction } from '../side-nav-action/side-nav-action';
 import { CartService } from '../feature/cart/services/cart-service';
 import { Counter } from '../services/counter';
 import { CounterComponent } from '../services/counter-component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-side-nav-bar',
@@ -17,6 +18,7 @@ export class SideNavBar {
 
   counterService = inject(Counter);
   counterComponentService = inject(CounterComponent);
+  numbers = signal<number[]>([]);
 
 
   onNavClick( title: any ) {
@@ -26,6 +28,12 @@ export class SideNavBar {
   public countIncrement() {
     this.counterService.increment();
     this.counterComponentService.increment();
+  }
+
+  public subscribe() {
+    this.counterService.getCounter().subscribe( count => {
+      this.numbers.update( value => [...value, count]);
+    })
   }
 
 }
